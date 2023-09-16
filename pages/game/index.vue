bd89e02f3eaf27ad3572e9ab1e5e12c904f93c8b
Tomasz Bawor <bawortomasz@gmail.com>
2023-09-16 17:29:57 +0200

    feat: add navbar

--- pages/game/index.vue
+++ pages/game/index.vue
@@ -1,17 +1,28 @@
 <script lang="ts" setup>
 import { useHeroStore } from '~/store/heroStore'
+import { definePageMeta } from '#imports'
+
+definePageMeta({
+  layout: false,
+})
 
 const heroStore = useHeroStore()
 </script>
 
 <template>
-  <header class="text-6xl text-center mt-10 font-headers">
-    Padron
-  </header>
+  <NuxtLayout name="game">
+    <nav class="bg-red-800 ">
+      <ul>
+        <li>Statistic</li>
+        <li>Inventory</li>
+        <li>Actions</li>
+      </ul>
+    </nav>
 
-  <pre>
+    <pre>
     {{ heroStore.hero }}
   </pre>
+  </NuxtLayout>
 </template>
 
 <style scoped></style>
